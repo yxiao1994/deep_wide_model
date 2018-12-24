0794f03a71b3f2a7faac11bad2a5430f4fbfe8e5
yxiao1994 <yxiao4715@gmail.com>
2018-12-24 15:21:26 +0800

    add deep wide model

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
 from src.data_parser import DataParser
 from src.model import deep_wide_model
+import argparse
+import pandas as pd
+
+if __name__ == '__main__':
+    df_train = pd.read_csv('df_train.csv')
+    dp = DataParser()
+    dfi_train, dfv_train, dfi_test, dfv_test, feature_dim, target = dp.generate_data()
--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class deep_wide_model(object):
     def __init__(self, feature_size, field_size, embedding_dim, dropout_rate, batch_size, loss_type='logloss',
-                 deep_layers=[64, 32], activation=tf.nn.relu, epoch=5):
+                 deep_layers=[64, 32], activation=tf.nn.relu, epoch=5, learning_rate=0.001):
         self.feature_size = feature_size  # denoted as N, feature size after one-hot encoding
         self.field_size = field_size  # denoted as F, feature size before one-hot encoding
         self.embedding_size = embedding_dim  # denoted as K, embedding size
@@ -14,16 +14,18 @@ class deep_wide_model(object):
         self.deep_layers = deep_layers
         self.activation = activation
         self.epoch = epoch
+        self.learning_rate = learning_rate
         self.train_result, self.valid_result = [], []
+        self._init_graph()
 
     def _init_weights(self):
         weights = dict()
         # wide部分权重，N * 1
         weights['wide'] = tf.Variable(tf.truncated_normal([self.feature_size, 1], 0, 0.01),
-                                      name='lr weights')
+                                      name='lr_weights')
         # deep部分权重，N * K
         weights['deep'] = tf.Variable(tf.truncated_normal([self.feature_size, 1], 0, 0.01),
-                                      name='embedding weights')
+                                      name='embedding_weights')
 
         # 全连接层的权重
         num_layers = len(self.deep_layers)
@@ -37,7 +39,7 @@ class deep_wide_model(object):
         for i in range(1, num_layers):
             glorot = np.sqrt(2.0 / (self.deep_layers[i - 1] + self.deep_layers[i]))
             weights['deep_layer_weights_%d' % i] = tf.Variable(
-                tf.random_normal([self.deep_layers[i - 1], self.deep_layers[0]], 0, glorot))
+                tf.random_normal([self.deep_layers[i - 1], self.deep_layers[i]], 0, glorot))
             weights['deep_layer_biase_%d' % i] = tf.Variable(
                 tf.random_normal([1, self.deep_layers[i]], 0, glorot))
 
@@ -47,34 +49,37 @@ class deep_wide_model(object):
         weights['concat_projection'] = tf.Variable(
             tf.random_normal([input_size, 1], 0, glorot))
         weights['concat_biase'] = tf.Variable(
-            tf.random_normal(tf.constant(0.01)), dtype=tf.float32)
+            tf.constant(0.01), dtype=tf.float32)
 
         return weights
 
     def _init_graph(self):
-        with tf.Graph.as_default():
+        g = tf.Graph()
+        with g.as_default():
             self.sess = tf.InteractiveSession()
-            self.feature_index = tf.placeholder(tf.int32, [None, self.field_size], name='feature index')
-            self.feature_value = tf.placeholder(tf.int32, [None, self.field_size], name='feature value')
+            self.feature_index = tf.placeholder(tf.int32, [None, self.field_size], name='feature_index')
+            self.feature_value = tf.placeholder(tf.float32, [None, self.field_size], name='feature_value')
             self.weights = self._init_weights()
             self.y = tf.placeholder(tf.int32, [None, 1], name='label')
             self.train_phase = tf.placeholder(tf.bool, name='train_phase')
 
             self.lr = tf.nn.embedding_lookup(self.weights['wide'], self.feature_index)  # None * F * 1
-            self.lr = tf.squeeze(self.lr)  # None * F
+
+            # self.lr = tf.squeeze(self.lr)  # None * F
+            self.lr = tf.reduce_sum(self.lr, axis=2)  # None * F
             self.lr = self.lr * self.feature_value  # None * F
 
             self.deep = tf.nn.embedding_lookup(self.weights['deep'], self.feature_index)  # None * F * K
             self.deep = tf.reshape(self.deep, [-1, self.embedding_size * self.field_size])  # None * (F * K)
+            # print(self.deep.shape)
             for i in range(len(self.deep_layers)):
-                self.deep = self.deep * self.weights['deep_layer_weights_%d' % i]
-                self.deep = self.deep + self.weights['deep_layer_biase_%d' % i]
+                self.deep = tf.add(tf.matmul(self.deep, self.weights['deep_layer_weights_%d' % i]),
+                                   self.weights['deep_layer_biase_%d' % i])
                 self.deep = self.activation(self.deep)
                 self.deep = tf.nn.dropout(self.deep, self.dropout_rate)
-
-            self.output = tf.concat([self.deep, self.lr], axis=1)
-            self.output = tf.add(tf.multiply(self.output, self.weights['concat_projection']),
-                                 self.weights['concat_biase'])
+            self.out = tf.concat([self.deep, self.lr], axis=1)
+            self.out = tf.add(tf.matmul(self.out, self.weights['concat_projection']),
+                              self.weights['concat_biase'])
 
             if self.loss_type == 'logloss':
                 self.out = tf.nn.sigmoid(self.out)
@@ -83,16 +88,7 @@ class deep_wide_model(object):
                 self.loss = tf.nn.l2_loss(tf.subtract(self.y, self.out))
 
             # optimizer
-            if self.optimizer_type == "adam":
-                self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
-            elif self.optimizer_type == "adagrad":
-                self.optimizer = tf.train.AdagradOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
-            elif self.optimizer_type == "gd":
-                self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).minimize(
-                    self.loss)
-            elif self.optimizer_type == "momentum":
-                self.optimizer = tf.train.MomentumOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
-
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
             tf.global_variables_initializer().run()
 
     def get_batch_data(self, Xi, Xv, label, index):
@@ -113,7 +109,7 @@ class deep_wide_model(object):
                      self.feat_value: Xv,
                      self.label: y,
                      self.train_phase: True}
-        loss, _ = self.sess.run([self.loss, self.optimizer])
+        loss, _ = self.sess.run([self.loss, self.optimizer], feed_dict=feed_dict)
         return loss
 
     def fit(self, Xi_train, Xv_tain, y_train, Xi_valid=None, Xv_valid=None, y_valid=None):
@@ -122,7 +118,7 @@ class deep_wide_model(object):
         for epoch in self.epoch:
             Xi_train, Xv_tain, y_train = self.shuffle_data(Xi_train, Xv_tain, y_train)
             for batch_index in total_batch:
-                Xi_batch, Xv_batch, y_batch = self.get_batch_data(Xi_train, Xv_tain, y_train)
+                Xi_batch, Xv_batch, y_batch = self.get_batch_data(Xi_train, Xv_tain, y_train, batch_index)
                 self.fit_batch_data(Xi_batch, Xv_batch, y_batch)
 
             train_result = self.evaluate(Xi_train, Xv_tain, y_train)
@@ -150,7 +146,7 @@ class deep_wide_model(object):
                          self.train_phase: False
                          }
             batch_res = self.sess.run(self.out, feed_dict=feed_dict)
-            y_predict = np.concatenate([y_predict, batch_res.reshape(-1,)])
+            y_predict = np.concatenate([y_predict, batch_res.reshape(-1, )])
 
         return y_predict
 
