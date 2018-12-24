6b0219f1a7058c3074f03db94e655c9201d52480
yxiao1994 <yxiao4715@gmail.com>
2018-12-24 17:05:51 +0800

    add deep wide model

--- src/model.py
+++ src/model.py
@@ -66,23 +66,19 @@ class deep_wide_model(object):
             self.train_phase = tf.placeholder(tf.bool, name='train_phase')
 
             self.lr = tf.nn.embedding_lookup(self.weights['wide'], self.feature_index)  # None * F * 1
-            # print(self.lr.shape)
-            # self.lr = tf.squeeze(self.lr)  # None * F
             self.lr = tf.reduce_sum(self.lr, axis=2)  # None * F
-            # print(self.lr.shape)
             self.lr = self.lr * self.feature_value  # None * F
-            # print(self.lr.shape)
 
             self.deep = tf.nn.embedding_lookup(self.weights['deep'], self.feature_index)  # None * F * K
             self.deep = tf.reshape(self.deep, [-1, self.embedding_size * self.field_size])  # None * (F * K)
-            # print(self.deep.shape)
+
             for i in range(len(self.deep_layers)):
                 self.deep = tf.add(tf.matmul(self.deep, self.weights['deep_layer_weights_%d' % i]),
                                    self.weights['deep_layer_biase_%d' % i])
                 self.deep = self.activation(self.deep)
                 self.deep = tf.nn.dropout(self.deep, self.dropout_rate)
             self.out = tf.concat([self.deep, self.lr], axis=1)
-            # print(self.out.shape)
+
             self.out = tf.add(tf.matmul(self.out, self.weights['concat_projection']),
                               self.weights['concat_biase'])
 
